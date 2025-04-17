3fdc27a3a439111b82d457764c9143f4e1dfbb25
Zihad Ahmed <zihad262940@gmail.com>
2025-04-17 21:24:00 +0600

    Update spamban.js

--- scripts/cmds/spamban.js
+++ scripts/cmds/spamban.js
@@ -1,4 +1,4 @@
-?cmd install spamban.js const axios = require("axios");
+const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const { createCanvas, loadImage } = require("canvas");
